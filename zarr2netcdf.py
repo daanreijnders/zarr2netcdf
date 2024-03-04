677638b602043be035b3774afe75d677e59e0c1d
daanreijnders <daanreijnders@outlook.com>
2024-03-04 11:39:46 +0100

    fixed bug with zlib encoding flag

--- zarr2netcdf.py
+++ zarr2netcdf.py
@@ -5,7 +5,7 @@ from dask.diagnostics import ProgressBar
 import argparse
 
 
-def zarr_to_netcdf(zarr_path, output_path=None, use_dask=False, verbose=False):
+def zarr_to_netcdf(zarr_path, output_path=None, use_dask=False, verbose=False, encoding=False, encoding_level=3):
     """
     Convert a .zarr store to a netCDF (.nc) file.
 
@@ -19,6 +19,10 @@ def zarr_to_netcdf(zarr_path, output_path=None, use_dask=False, verbose=False):
         Use dask parallelization. Enables progress bar.
     verbose : bool, optional
         Print progress messages.
+    encoding : bool, optional
+        Compress the output file using zlib.
+    encoding_level : int, optional
+        Compression level for the output file.
     """
     # Check if the provided path exists
     if not os.path.exists(zarr_path):
@@ -40,12 +44,16 @@ def zarr_to_netcdf(zarr_path, output_path=None, use_dask=False, verbose=False):
     if output_path is None:
         output_path = os.path.splitext(zarr_path)[0] + '.nc'
 
+    encoding_dict = None
+    if encoding:
+        encoding_dict = {key: {"zlib": True, "complevel": encoding_level} for key in ds.data_vars}
+
     # Save the xarray dataset as a netCDF (.nc) file with progress bar
     if use_dask:
         with ProgressBar():
-            ds.to_netcdf(output_path, compute=True)
+            ds.to_netcdf(output_path, compute=True, enconding=encoding_dict)
     else:
-        ds.to_netcdf(output_path)
+        ds.to_netcdf(output_path, enconding=encoding_dict)
 
     if verbose:
         print(f"Converted {zarr_path} to {output_path}")
@@ -58,8 +66,10 @@ def main():
         "--output", "-o", help="Path to save the output netCDF file. If not provided, the output will have the same name as the input with a .nc extension.", default=None)
     parser.add_argument("--use_dask", "-d", help="Use dask parallelization. Enables progress bar.", action="store_true")
     parser.add_argument("--verbose", "-v", help="Print progress messages.", action="store_true")
+    parser.add_argument("--encoding", "-e", action="store_true", help="Compress the output file using zlib.")
+    parser.add_argument("--encoding_level", "-l", type=int, default=3, help="Compression level for the output file.")
     args = parser.parse_args()
-    zarr_to_netcdf(args.zarr_path, args.output, use_dask=args.use_dask, verbose=args.verbose)
+    zarr_to_netcdf(args.zarr_path, args.output, use_dask=args.use_dask, verbose=args.verbose, encoding=args.encoding, encoding_level=args.encoding_level)
 
 
 if __name__ == "__main__":
